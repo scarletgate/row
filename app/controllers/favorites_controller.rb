4101f43190da36008ec4cea8f08334cbf059ef02
scarletgate <xiaoyuedu10@gmail.com>
2021-12-16 01:35:31 +0000

    [Add]コメント機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    favorite = Favorite.new(post_id: @post.id)
+    #favorite = current_user.favorites.new(post_image_id: post_image.id)
+    favorite = Favorite.new(post_id: @post.id)#post_idカラムに@post.idを入れた空のモデルをfavoriteに渡す。
     favorite.user_id = current_user.id
     favorite.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    #favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite = Favorite.find_by(post_id: @post.id)
     favorite.user_id = current_user.id
     favorite.destroy
@@ -16,7 +18,7 @@ class FavoritesController < ApplicationController
   #いいね一覧
   def index
     @user = User.find(params[:user_id])
-    @users = @user.favorites
+    @favorites = @user.favorites
   end
 
 end
