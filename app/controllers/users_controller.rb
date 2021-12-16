b271a389de247526e9bca8a79563af1ac662974e
scarletgate <xiaoyuedu10@gmail.com>
2021-12-16 05:51:16 +0000

    [Fix]画面レイアウトを微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def withdraw
+  def withdrawal
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました。"
+    redirect_to root_path
   end
 
   private
